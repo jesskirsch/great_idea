6155c402e57b7faf572675c48ebffc2cc7f26eaa
Jessica Kirsch <jessicaruthkirsch@gmail.com>
2017-03-19 22:40:58 -0400

    second round of design

--- app/assets/javascripts/ideas.js
+++ app/assets/javascripts/ideas.js
@@ -1,2 +1,9 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).ready(function() {
+$('.text').hide(); //Initially form wil be hidden.
+$('.title-box').click(function() {
+ $('.text').show();//Form shows on button click
+
+  });
+});
